1ede815efef6ad70b35ff74b14e767c9ed4a3bc6
kavindhagihan5-crypto <kavindhagihan5@gmail.com>
2025-09-05 12:29:06 +0530

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,6 +1,6 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
+  SESSION_ID: "malvin~9vl3CDKT#mwUz02YRYPUS2Vmw6Dl8ThNzwkqbyqiC0NirLR0DXpY",  // add your Session Id here
   
   OWNER_NUMBER: "263714757857", // put your phone number here
   
